55c6888363ef2469a7e2c351609fce399051fa4e
Daniel Pujazon Bonet <danielpujazonbonet@gmail.com>
2023-04-10 16:49:19 +0200

    Use Pear HTML_Table to insert html tables into php

--- src/accessDB.php
+++ src/accessDB.php
@@ -1,9 +1,11 @@
 <?php
+require_once 'libs/HTML/Table.php';
+
 if(isset($_POST['allDb'])) {
     $servername = "localhost";
-    $username = "X";
-    $password = "Y";
-    $database = "Z";
+    $username = "root";
+    $password = "";
+    $database = "tsanalitics";
     
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
@@ -15,14 +17,42 @@ if(isset($_POST['allDb'])) {
     echo "Connected successfully";
     echo "<br>";
 
+    $attrs = array('width' => '600');
+    $table = new HTML_Table($attrs);
+    $table->setAutoGrow(true);
+    $table->setAutoFill('n/a');
+
+    $table->setHeaderContents(0, 0, '');
+    $table->setHeaderContents(0, 1, 'Jira ticket');
+    $table->setHeaderContents(0, 2, 'Developer');
+    $table->setHeaderContents(0, 3, 'Project');
+    $table->setHeaderContents(0, 4, 'Sprint');
+    $table->setHeaderContents(0, 5, 'Story Points');
+    $table->setHeaderContents(0, 6, 'Log Work');
+    $table->setHeaderContents(0, 7, 'Complexity');
+    $table->setHeaderContents(0, 8, 'Skill');
+    $hrAttrs = array('bgcolor' => 'silver');
+    $table->setRowAttributes(0, $hrAttrs, true);
+    $table->setColAttributes(0, $hrAttrs);
+
     $result = mysqli_query($conn, "SELECT * FROM tickets") or die ("Could not search");
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "SCXBB-", $row['id'], ' | ', $row['developer'],' | ';
-        echo $row['project'], ' | ', $row['sprint'],' | ';
-        echo $row['sp'], ' | ', $row['lw'],' | ';
-        echo $row['complexity'], ' | ', $row['skill'],' | ';
-        echo $row['info'],' | ';
-        echo "<br>";
+    $data= mysqli_fetch_array($result);
+
+    for ($nr = 0; $nr < count($data); $nr++) {
+      //TODO: Parse must be modified
+      $data= mysqli_fetch_array($result);
+      //DBG
+      echo $data[0];
+      $table->setHeaderContents($nr+1, 0, (string)$nr);
+      for ($i = 0; $i < 4; $i++) {
+        if ('' != $data[$nr][$i]) {
+          $table->setCellContents($nr+1, $i+1, $data[$nr][$i]);
+        }
+      }
     }
+    $altRow = array('bgcolor' => 'red');
+    $table->altRowAttributes(1, null, $altRow);
+
+    echo $table->toHtml();
 }
 ?>
